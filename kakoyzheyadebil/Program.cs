cfaf1886ebfe3785f06c59fab4b7783cf0dbd72c
Larik1329 <55159766+Larik1329@users.noreply.github.com>
2023-10-10 21:47:44 +0300

    Add css

--- kakoyzheyadebil/Program.cs
+++ kakoyzheyadebil/Program.cs
@@ -19,6 +19,8 @@ namespace kakoyzheyadebil
             var app = builder.Build();
             app.UseHttpsRedirection();
 
+            app.UseStaticFiles();
+            
             app.UseAuthentication();
             app.UseAuthorization();
 
